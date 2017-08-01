d8bce1f9fbe94a1ccde466d7d6de4bf480d73be9
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-01 14:12:48 -0400

    added a destructuring example using objects created from variables

--- es6-mortgage-calculator/js/main.js
+++ es6-mortgage-calculator/js/main.js
@@ -4,13 +4,15 @@ let calculateMonthlyPayment = function (principal, years, rate) {
         monthlyRate = rate / 100 / 12;
     }
     let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
+    // return monthlyPayment;
+    return {principal, years, rate, monthlyPayment, monthlyRate};
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
 });
