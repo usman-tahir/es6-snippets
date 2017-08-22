29d3dc956c1eb62d0927dbe199a3122557ab7684
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-22 19:10:33 -0400

    added an ES6 snippet for scoped variables/hoisting

--- code-snippets/js/index.js
+++ code-snippets/js/index.js
@@ -4,4 +4,15 @@
   // Constants
   const PI = 3.141593
   console.log(PI > 3.0) // true
+
+  // Scope (block-scoped variables/constants without hoisting)
+  let callbacks = [];
+  for (let i = 0; i <= 2; i += 1) {
+    callbacks[i] = function() { return i * 2 }
+  }
+
+  // Check the values
+  console.log(callbacks[0]() === 0)
+  console.log(callbacks[1]() === 2)
+  console.log(callbacks[2] === 4)
 }())
