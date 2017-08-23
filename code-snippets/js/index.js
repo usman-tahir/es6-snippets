f6c9a50fa7bb03080347409f1eff4fd1b2cf9680
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-23 19:14:30 -0400

    added an ES6 snippet for RegEx matching

--- code-snippets/js/index.js
+++ code-snippets/js/index.js
@@ -102,4 +102,30 @@
   // Extended literals
   console.log(0b111110111 === 503)
   console.log(0o767 === 503)
+
+  // Extended regular expressions (sticky matching)
+  let parser = (input, match) => {
+    for (let pos = 0, lastPos = input.length; pos < lastPos;) {
+      for (let i = 0; i < match.length; i += 1) {
+        match[i].pattern.lastIndex = pos
+        let found
+        if ((found = match[i].pattern.exec(input)) !== null) {
+          match[i].action(found)
+          pos = match[i].pattern.lastIndex
+          break
+        }
+      }
+    }
+  }
+
+  let report = (match) => {
+    console.log(JSON.stringify(match))
+  }
+
+  parser("Foo 1 Bar 7 Baz 42", [
+    { pattern: /^Foo\s+(\d+)/y, action: (match) => report(match) },
+    { pattern: /^Bar\s+(\d+)/y, action: (match) => report(match) },
+    { pattern: /^Baz\s+(\d+)/y, action: (match) => report(match) },
+    { pattern: /^\s*/y, action: (match) => {} }
+  ])
 }())
